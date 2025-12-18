ab4f4be4600d9949245a9d28c173dfb368d9f665
Shakthi Indra Varma <sakthiindravarma@gmail.com>
2025-12-18 21:56:58 +0530

    Update API endpoint for note creation

--- frontend/src/Pages/CreatePage.jsx
+++ frontend/src/Pages/CreatePage.jsx
@@ -14,7 +14,7 @@ const CreatePage = () => {
     // Handling form submission logic here
 
     if (title && content) {
-      await axios.post("http://localhost:5000/notes", { title, content });
+      await axios.post("https://to-do-j69k.onrender.com", { title, content });
       toast.success("Note created successfully!");
       navigate("/");
       // Resetting form fields
